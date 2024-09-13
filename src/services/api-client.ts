194f5bf97f17ab3a04cd8f063ce0e5ff9e742459
skyfa <skinthav@millersville.edu>
2024-09-13 11:55:25 -0400

    Create custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+// Note will change the managing of ths api key (not secure)
+// Temporary until custom backend server can be used as a proxy
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
