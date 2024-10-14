b3048fca06943c8313520a173d7af917edc8937f
skyfa <skinthav@millersville.edu>
2024-10-14 13:18:09 -0400

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -29,7 +30,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       {/* make sure only renders on large */}
       <Show above="lg">
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,12 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
+
 export interface Game {
     id: number;
     name: string;
@@ -20,7 +20,8 @@ const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
     params: { 
         genres: gameQuery.genre?.id, 
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
     }
 }, 
     [gameQuery]);
